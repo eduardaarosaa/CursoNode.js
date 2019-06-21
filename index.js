e8e08dc01d0e43b358fb818a0ec598679a41546d
Duda <eduardacirina@gmail.com>
2019-06-21 12:54:24 -0300

    Criando rotas

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ var app = express(); //cria uma instancia - uma cópia do framework express
 app.get("/", function(req,res){
     res.send("Seja bem vindo");
 })
+//req é a requisicao e res é a mensagem para o cliente
+//criando uma outra rota para uma outra página
+app.get("/sobre", function(req,res){
+    res.send("Minha página sobre");
+})
+
+//criando uma outra rota blog 
+app.get("/blog",function(req,res){
+    res.send("Bem vindo ao blog");
+})
+// quando coloca /: é pq é um parametro
+app.get('/ola/:cargo/:nome', function(req,res){
+    res.send(req.params);
+//mostrando os parametros para o usuario
+})
+app.get('/home/:escola/:idade',function(req,res){
+    res.send("Essa é a escola "+ req.params.escola + " Os alunos tem aproximadamente "+req.params.idade + " anos");
+
+})
 //sempre deve ser a ultima função do código
 //Função de calback - Função é execultada atraves de um evento.
 app.listen(8081, function(){
